c908ae019f86a3496d5b44f4e15b112d74a449df
areeba44 <110978549+areesha-22@users.noreply.github.com>
2024-12-05 11:19:06 +0500

    ecommerce

---
+++ app/src/Components/Nav.tsx
@@ -0,0 +1,25 @@
+// nav
+// slider
+// most popular product any 2
+//  new arrivals
+// different pages mulit-vitamins ,viral, All products
+// add to card
+//contact page
+
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <div className='bg-black '>
+      <div className=" text-white font-normal text-sm  text-center justify-center  ">
+      Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!  
+
+      <span className='font-semibold text-sm '>    ShopNow</span>
+      <div className='font-normal text-sm'>English</div>
+      </div>
+    </div></div>
+  )
+}
+
+export default Nav
---
+++ app/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Nav from "../Components/Nav";
+export default function Home() {
+  return (
+   
+      <div>
+        <Nav/>
+      </div>
+         
+
+  );
+}
